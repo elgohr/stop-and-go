5fbcd083450cc3fb16a35855a7c6af1483e8c70d
Lars Gohr <elgohr@oss.volkswagen.com>
2020-11-06 10:15:08 +0100

    :pencil: Add example

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # stop-and-go
 Testing helper for concurrency
+
+## Usage
+
+```go
+import "github.com/stretchr/testify/require"
+
+func TestExample(t *testing.T) {
+	w1 := wait.NewWaiter(time.Second)
+	w2 := wait.NewWaiter(time.Second)
+	w3 := wait.NewWaiter(time.Second)
+
+	ts1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w2.Done()
+	}))
+	defer ts1.Close()
+
+	go func() {
+		w3.Done()
+	}()
+
+	go func() {
+		_, err := http.Get(ts1.URL)
+		require.NoError(t, err)
+		w1.Done()
+	}()
+
+	require.NoError(t, wait.For(
+		constraint.NoOrder(w3),
+		constraint.Before(w1, w2),
+	))
+}
+```
---
+++ wait/wait_example_test.go
@@ -0,0 +1,48 @@
+package wait_test
+
+import (
+	"fmt"
+	"github.com/elgohr/stop-and-go/constraint"
+	"github.com/elgohr/stop-and-go/wait"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"time"
+)
+
+func ExampleFor() {
+	w1 := wait.NewWaiter(time.Second)
+	w2 := wait.NewWaiter(time.Second)
+	w3 := wait.NewWaiter(time.Second)
+
+	ts1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w2.Done()
+	}))
+	defer ts1.Close()
+
+	go func() {
+		w3.Done()
+	}()
+
+	go func() {
+		_, err := http.Get(ts1.URL)
+		if err != nil{
+			log.Fatalln(err)
+		}
+		w1.Done()
+	}()
+
+	fmt.Println(wait.For(
+		constraint.NoOrder(w3),
+		constraint.Before(w1, w2),
+	))
+	// Output: <nil>
+}
+
+func ExampleFailing() {
+	w1 := wait.NewWaiter(time.Second)
+	fmt.Println(wait.For(constraint.NoOrder(w1)))
+	// Output: failed to wait on waiter 1 of 1
+}
+
